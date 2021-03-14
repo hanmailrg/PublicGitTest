b8f3934bff95aab75af3cd8d159914d225fa8c34
seoes <seoes@DESKTOP-58226D0>
2021-03-14 13:06:51 +0900

    팀원 Seoes가 물품관리 자바소스 파일 및 Html 파일 생성함.

---
+++ PublicGitTest/src/seoes/test01/SeoesTest01.java
@@ -0,0 +1,10 @@
+package seoes.test01;
+
+public class SeoesTest01 {
+
+	public static void main(String[] args) {
+		// 팀원 seoes 가 작업한 것임.
+		System.out.println("연습용 : 팀원 seoes 가 작업한 것임.");
+	}
+
+}
